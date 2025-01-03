138659f975f0700dd692180372c71c47f7484921
riz.it <fachrudin.mochamad@student.unmer.ac.id>
2025-01-03 11:18:08 +0700

    [ADD] Setup Pin Recovery

--- internal/dto/pin_recovery.go
+++ internal/dto/pin_recovery.go
@@ -1,1 +1,9 @@
 package dto
+
+// Request
+type SetupWalletPINRequest struct {
+	WalletID int64  `json:"wallet_id"`
+	PinCode  string `json:"pin_code"`
+}
+
+// Response
--- internal/usecase/pin_recovery_usecase.go
+++ internal/usecase/pin_recovery_usecase.go
@@ -1,1 +1,32 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+	"riz.it/domped/internal/domain"
+	"riz.it/domped/internal/dto"
+)
+
+type PinRecoveryUsecase struct {
+	DB               *gorm.DB
+	Log              *logrus.Logger
+	WalletRepository domain.WalletRepository
+	Validate         *validator.Validate
+}
+
+func NewPinRecoveryUsecase(db *gorm.DB, log *logrus.Logger, walletRepository domain.WalletRepository, validate *validator.Validate) domain.PinRecoveryUseCase {
+	return &PinRecoveryUsecase{
+		DB:               db,
+		Log:              log,
+		WalletRepository: walletRepository,
+		Validate:         validate,
+	}
+}
+
+// SetupWalletPIN implements domain.PinRecoveryUseCase.
+func (p *PinRecoveryUsecase) SetupWalletPIN(ctx context.Context, req *dto.SetupWalletPINRequest) error {
+	panic("unimplemented")
+}
