e243c60b16e42ba399343f281d9aae557a33ca57
riz.it <fachrudin.mochamad@student.unmer.ac.id>
2025-01-10 10:24:37 +0700

    [ADD vercel deploy

---
+++ api/index.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"riz.it/domped/internal/injector"
+)
+
+// Handler is the main entry point of the application. Think of it like the main() method
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// This is needed to set the proper request path in `*fiber.Ctx`
+	r.RequestURI = r.URL.String()
+
+	handler().ServeHTTP(w, r)
+}
+
+// building the fiber application
+func handler() http.HandlerFunc {
+	app := injector.InitializedApp()
+	return adaptor.FiberApp(app.Fiber)
+}
